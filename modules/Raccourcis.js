ab47ec2732b1d49b96eb1d893bbdf2e0815b048b
Superdrac <superdrac@gmail.com>
2014-08-14 17:55:19 +0200

    Ajout Fct Page suivante

--- modules/Raccourcis.js
+++ modules/Raccourcis.js
@@ -3,37 +3,18 @@
 /* jshint newcap: false */
 
 /**
- * Raccourcis: Remplace les liens Raccourcis par le player embed fourni par le site
+ * Raccourcis: Ajoute des raccourcis aux forums
  */
 SK.moduleConstructors.Raccourcis = SK.Module.new();
 
 SK.moduleConstructors.Raccourcis.prototype.title = "Intégration Raccourcis";
-SK.moduleConstructors.Raccourcis.prototype.description = "Remplace les liens Raccourcis par le player embed fourni par le site.";
+SK.moduleConstructors.Raccourcis.prototype.description = "Ajoute des raccourcis clavier";
 
 SK.moduleConstructors.Raccourcis.prototype.init = function() {
-    /**
-     * Fonction qui analyse un lien. Si il correspond à un Raccourcis, il retourne une version "embed" du vovaroo.
-     */
-    function normaRaccourcis(url) {
-	
-        if(url.indexOf('Raccourcis.com/i/')!=-1){
-			var splitVal=url.split('/i/');
-			if(splitVal[1].length==12){
-				return "http://Raccourcis.com/player.swf?playMediaID="+splitVal[1]+"&autoplay=0";
-			}
-			else{
-				return null;
-			}
-        }
-		else{
-			return null;
-		}
-    }
 	/**
 	*Fonction de passage à la page précedente
 	*/
 	function pPrecedente(){
-		alert('pPrecedente in fct');
 		var path=window.location.href;
 		var splitLoca=path.split('-');
 		if(splitLoca[3]>1){
@@ -52,35 +33,30 @@ SK.moduleConstructors.Raccourcis.prototype.init = function() {
 			window.location.href=urlFinale;
 		}
 	}
-    /**
-     * Fonction qui crée l'élément à intégrer à la page.
-     */
-    function createEmbededPlayer(vocaLink) {
-            var $el = $("<object>");
-			var $param1=$("<param>");
-			var $param2=$("<param>");
-			var $embed=$("<embed>");
-			//Element Object
-            $el
-                .attr("width", "400")
-                .attr("height", "44");
-			$param1
-					.attr("name", "movie")
-					.attr("value", vocaLink);
-			$param2
-					.attr("name","wmode")
-					.attr("value", "transparent");
-			$embed
-					.attr("src", vocaLink)
-					.attr("width", "148")
-					.attr("height", "44")
-					.attr("wmode", "transparent")
-					.attr("type", "application/x-shockwave-flash");
-			$el.append($param1);
-			$el.append($param2);
-			$el.append($embed);
-            return $el;
-        }
+	/**
+	*Fonction de passage à la page suivante
+	*/
+	function pSuivante(){
+		var path=window.location.href;
+		var splitLoca=path.split('-');
+		var nbPageMax=parseInt($('.pagination').eq(0).find('a').eq($('.pagination').eq(0).find('a').length-1)[0].innerHTML);
+		if(splitLoca[3]<nbPageMax){
+			splitLoca[3]=parseInt(splitLoca[3])+1;
+			splitLoca[3]=splitLoca[3].toString();
+			var nbSegment=splitLoca.length;
+			var urlFinale="";
+			for(var i=0;i<nbSegment;i++)
+			{	if(i!=nbSegment-1){
+					urlFinale+=splitLoca[i]+"-";
+				}
+				else{
+					urlFinale+=splitLoca[i];
+				}
+			}
+			window.location.href=urlFinale;
+		}
+	}
+    
         
   
     /**
@@ -89,14 +65,13 @@ SK.moduleConstructors.Raccourcis.prototype.init = function() {
 		$(window).keydown(function(event) {
 			//Ctrl + fleche gauche -> page précedente
 			if (event.ctrlKey && event.keyCode == 37) {
-				alert('pPrecedente');
 				pPrecedente();
 				event.preventDefault();
 			}
 			//Ctrl + fleche doite -> page suivante
 			if (event.ctrlKey && event.keyCode == 39) {
 				pSuivante();
-				//event.preventDefault();
+				event.preventDefault();
 			}
 		});
 };
