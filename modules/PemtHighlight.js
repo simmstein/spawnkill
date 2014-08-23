79c246957a457ed389fdd70f529bab23bf5b76f6
nounoursheureux <schtroumps31@gmail.com>
2014-08-23 10:29:09 +0200

    Ajout du module PemtHighlight.js

---
+++ modules/PemtHighlight.js
@@ -0,0 +1,59 @@
+"use strict";
+/* jshint multistr: true */
+/* jshint newcap: false */
+
+/**
+ * NouveauModule : Description du module
+ */
+SK.moduleConstructors.NouveauModule = SK.Module.new();
+
+SK.moduleConstructors.NouveauModule.prototype.id = "NouveauModule";
+SK.moduleConstructors.NouveauModule.prototype.title = "Titre du nouveau module";
+SK.moduleConstructors.NouveauModule.prototype.description = "Description du nouveau module";
+SK.moduleConstructors.NouveauModule.prototype.required = true;
+
+/**
+ * Initialise le module, fonction appelée quand le module est chargé
+ */
+SK.moduleConstructors.NouveauModule.prototype.init = function() {
+    var dates = $('.date').html().sort();
+    var results = [];
+    for (var i = 0; i < dates.length -i; i++) {
+        if (dates[i] == dates[i+1]) {
+            results.push(dates[i]);
+        }
+    }
+    if (0 < array.length) {
+        $('.msg').find('.date:contains('+dates[i]+')').parent('.msg').attr('background','red');
+    }   
+};
+
+SK.moduleConstructors.NouveauModule.prototype.uneMethodeExemple = function() {
+    //Ma méthode
+};
+
+/**
+ * Méthode testant si un Module doit être activé.
+ * peut-être redéfinie.
+ * Par défaut le module est toujours activé
+ */
+SK.moduleConstructors.NouveauModule.prototype.shouldBeActivated = function() {
+    return true;
+};
+
+/**
+ * Retourne le CSS à injecter si le plugin est activé.
+ * Par défaut, aucun CSS n'est injecté.
+ */
+SK.moduleConstructors.NouveauModule.prototype.getCss = function() {
+
+	var css = "";
+
+    return css;
+};
+
+/**
+ * Options configurables du plugin.
+ * Ces options apparaitront dans le panneau de configuration de SpawnKill
+ */ 
+SK.moduleConstructors.NouveauModule.prototype.settings = {};
